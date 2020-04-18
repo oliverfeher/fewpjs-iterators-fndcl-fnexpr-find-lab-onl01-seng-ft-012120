5b9bdc70f21e748985fb45ae2cfabb7143139fb3
oliverfeher <oliver92.feher@gmail.com>
2020-04-17 22:57:09 -0400

    Done.

--- index.js
+++ index.js
@@ -3,3 +3,11 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+
+const superbowlWin = (arr) => {
+  if (!arr.find( e => e.result === "W")) {
+    return undefined
+  } else {
+    return arr.find( e => e.result === "W").year
+  }
+}
